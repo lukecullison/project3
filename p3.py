7405f20a884d28a4db007546227f1cad7d168b9a
Luke Cullison <lec99@vt.edu>
2020-12-02 22:20:38 -0500

    implementation tentatively finished

--- p3.py
+++ p3.py
@@ -25,12 +25,15 @@ def readFile(infile, pattern):
                 col = 0
             lin += 1
         print(infile + ": found " + str(count) + " matching pattern(s)")
+        return count
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print('Usage: python3 p3.py "pattern" "file1" "file2" [-c]')
         exit()
     i = 2
+    pcount = 0
     if sys.argv[len(sys.argv) - 1] == '-c':
+        changed = False
         while(i < len(sys.argv) - 1):
             p = os.access(sys.argv[i], os.R_OK)
             mod = False
@@ -41,18 +44,23 @@ if __name__ == '__main__':
                 mod = True
             p = os.access(sys.argv[i], os.R_OK)
             if p == True:
-                readFile(sys.argv[i], sys.argv[1])
+                pcount += readFile(sys.argv[i], sys.argv[1])
                 if mod:
+                    changed = True
                     print(sys.argv[i] + ": Changed file read permissions for owner, group, and other")
             else:
                 print("Invalid file name or file path")
             i += 1
+        if changed == False:
+            print("No file permissions were modified")
+        print("Summary: " + str(pcount) + " matche(s) found in " + str(len(sys.argv) - 3) + " files")
         exit()
     while(i < len(sys.argv)):
         p = os.access(sys.argv[i], os.R_OK)
         if p == True:
-            readFile(sys.argv[i], sys.argv[1])
+            pcount += readFile(sys.argv[i], sys.argv[1])
         else:
             print("Invalid file name or file path")
         i += 1
     print("No file permissions were modified")
+    print("Summary: " + str(pcount) + " matche(s) found in " + str(len(sys.argv) - 2) + " files")
